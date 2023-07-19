f4fcdf84c7ed06f6620c48be2a43436064a36b47
nicholas <22014726@myrp.edu.sg>
2023-07-19 14:20:36 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22014726/demotodolist/ToDoItem.java
@@ -0,0 +1,70 @@
+package sg.edu.rp.c346.id22014726.demotodolist;
+
+import java.util.Calendar;
+
+public class ToDoItem {
+
+    private String title;
+    private Calendar date;
+
+    public ToDoItem(String title, Calendar date) {
+        this.title = title;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+
+    public String getDateString() {
+        String str = date.get(Calendar.DAY_OF_MONTH) + "/" + date.get(Calendar.MONTH)
+                + "/" + date.get(Calendar.YEAR) + " (" + getDay(date.get(Calendar.DAY_OF_WEEK)) + ")";
+    return str;
+
+    }
+
+    private String getDay(int day) {
+        String dayString;
+
+        switch (day) {
+            case 1:
+                dayString = "Sunday";
+                break;
+            case 2:
+                dayString = "Monday";
+                break;
+            case 3:
+                dayString = "Tuesday";
+                break;
+            case 4:
+                dayString = "Wednesday";
+                break;
+            case 5:
+                dayString = "Thursday";
+                break;
+            case 6:
+                dayString = "Friday";
+                break;
+            case 7:
+                dayString = "Saturday";
+                break;
+            default:
+                dayString = "";
+                break;
+        }
+
+        return dayString;
+    }
+}
